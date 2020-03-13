83c77c17867c2fa5521e4a1e100f6fbbed13506f
matt019 <mmaybon@css.edu>
2020-03-12 22:58:06 -0400

    test
    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Home = () => {
         return(
             <div className="jumbotron">
-                <h1>Library Management System</h1>
+                <h1>Test</h1>
             </div>
         );
 }
