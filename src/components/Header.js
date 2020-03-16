b912529660ef07c97ec97241c8c1b058a29b8613
matt019 <mmaybon@css.edu>
2020-03-16 02:57:07 -0400

    changes
    added new logo, tabs, and changed fonts/colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,36 @@
 "use strict"
 
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-        return(
-            <nav className="navbar navbar-inverse">
-                <div className="container-fluid">
-                    <ul className="list-inline">
-                        <li className="list-inline-item"> 
-                            <Link to="/" className="navbar-brand">
-                                <img width="60px" height="90px" src="images/handicon.png" />
-                            </Link>
-                        </li>
+    return (
+
+        <div className="jumbotron">
+        <div className="main">
+            <h1>
+                <img width="200px" height="30px" src="images/sslogo.png" />
+            </h1>
+            <h1 align="center"> USPS Dynamics Project
+            </h1>
+            </div>
+            <div className="tabs"></div>
+            <nav>
+                <ul className="list-inline">
+                    <h5 align="center">
                         <li className="list-inline-item"><Link to="/" replace>Home</Link></li>
                         <li className="list-inline-item"><Link to="/books" replace>Books</Link></li>
-                        <li className="list-inline-item"><Link to="/Application" replace>Application</Link></li>
-                    </ul>
-                </div>
+                        <li className="list-inline-item"><Link to="/Application" replace>Completed Applications</Link></li>
+                        <li className="list-inline-item"><Link to="/customer" replace>Customers</Link></li>
+                        <li className="list-inline-item"><Link to="/newapp" replace>New Application</Link></li>
+
+                    </h5>
+                </ul>
             </nav>
-        );
+
+        </div>
+
+    );
 }
 
 export default Header;
