9917ad5e07f15f6d7c5f8995613696d6978f9cc5
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2020-03-26 02:24:01 -0400

    retrieve and store reference entities

--- src/reducers/applicationsReducer.js
+++ src/reducers/applicationsReducer.js
@@ -1,16 +1,17 @@
-import { READ_APPLICATIONS_SUCCESFUL, READ_APPLICATIONS_PENDING, READ_APPLICATIONS_FAILURE, DELETE_APPLICATIONS_SUCCESFUL, DELETE_APPLICATIONS_FAILURE } from '../constants/actionTypes';
+import { READ_APPLICATIONS_SUCCESSFUL, READ_APPLICATIONS_PENDING, READ_APPLICATIONS_FAILURE, DELETE_APPLICATION_SUCCESSFUL, DELETE_APPLICATION_FAILURE } from '../constants/actionTypes';
 
 export default function applicationsReducer(state = {}, action) {
+
     switch (action.type) {
-        case READ_APPLICATIONS_SUCCESFUL:
+        case READ_APPLICATIONS_SUCCESSFUL:
             return { ...state, applications: action.data.value, applicationsRequestSuccess: true, applicationsRequestPending: false, applicationsRequestFailed: false };
         case READ_APPLICATIONS_PENDING:
             return { ...state, applicationsRequestSuccess: false, applicationsRequestPending: true, applicationsRequestFailed: false };
         case READ_APPLICATIONS_FAILURE:
             return { ...state, applicationsRequestSuccess: false, applicationsRequestPending: false, applicationsRequestFailed: true };
-        case DELETE_APPLICATIONS_SUCCESFUL:
-            return { ...state, applications: action.data.value, applicationsRequestSuccess: true, applicationsRequestPending: false, applicationsRequestFailed: false };
-        case DELETE_APPLICATIONS_FAILURE:
+        case DELETE_APPLICATION_SUCCESSFUL:
+            return { ...state, applications: state.applications.filter((e) => e.ss_applicationid !== action.data), applicationsRequestSuccess: true, applicationsRequestPending: false, applicationsRequestFailed: false };
+        case DELETE_APPLICATION_FAILURE:
             return { ...state, applicationsRequestSuccess: false, applicationsRequestPending: false, applicationsRequestFailed: true };
         default:
             return state;
