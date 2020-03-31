780bc8991e7b911d5033e3b1ba82abcf4c4955d3
enfenry <enfenry@vt.edu>
2020-03-31 15:46:59 -0400

    AppUpdate validation complete. will cleanup and refactor before starting AppAdd

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,19 @@
 import { combineReducers } from 'redux';
-import * as entityReducers from '..reducers';
+import applicationsReducer from './applicationsReducer';
+import customersReducer from './customersReducer';
+import ordersReducer from './ordersReducer';
+import productsReducer from './productsReducer';
+import addressesReducer from './addressesReducer';
 
 import { reducer as modal } from 'redux-modal';
 import { reducer as formReducer } from 'redux-form';
 
-const rootReducer = combineReducers({
-  entityReducers,
+ const rootReducer = combineReducers({
+  applicationsReducer,
+  customersReducer,
+  ordersReducer,
+  productsReducer,
+  addressesReducer,
   modal,
   form: formReducer
 });
