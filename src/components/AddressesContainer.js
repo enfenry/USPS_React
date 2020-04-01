55d728fb51cbdc42b0d72c8f4445643d1b880381
matt019 <mmaybon@css.edu>
2020-04-01 18:18:11 -0400

    add

---
+++ src/components/AddressesContainer.js
@@ -0,0 +1,97 @@
+"use strict"
+
+import * as applicationsActions from '../actions/applicationsActions';
+import ApplicationsRender from './ApplicationsRender';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+const ApplicationsContainer = (props) => {
+
+    if (props.applicationsRequestPending || props.ordersRequestPending || props.productsRequestPending || props.customersRequestPending || props.addressesRequestPending) {
+        return (
+            <div className="d-flex justify-content-center">
+                <div className="spinner-border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </div>
+            </div>
+        );
+    } else if (props.applicationsRequestFailed || props.ordersRequestFailed || props.productsRequestFailed || props.customersRequestFailed || props.addressesRequestFailed) {
+        return (
+            <div className="alert alert-danger" role="alert">
+                Error while loading entities!
+            </div>
+        );
+
+    } else if (props.applicationsRequestSuccess && props.ordersRequestSuccess && props.productsRequestSuccess && props.customersRequestSuccess && props.addressesRequestSuccess) {
+        return (
+            <div className="reactive-margin">
+                <ApplicationsRender
+                    applications={props.applications}
+                    handleUpdate={(values, application) => {
+                        props.actions.updateApplication(values, application.ss_applicationid)
+                    }}
+                    handleDelete={application => {
+                        props.actions.deleteApplication(application.ss_applicationid)
+                    }}
+                    handleCreate={(values) => {
+                        props.actions.createApplication(values)
+                    }}
+                />
+            </div>
+        );
+    } else {
+        return (
+            <div className="alert alert-danger" role="alert">
+                Invalid state! This message should never appear.
+            </div>
+        );
+    }
+}
+
+
+ApplicationsContainer.propTypes = {
+    actions: PropTypes.object
+};
+
+function mapStateToProps(state) {
+    return {
+        applications: state.applicationsReducer.applications,
+        products: state.productsReducer.products,
+        orders: state.ordersReducer.orders,
+        customers: state.customersReducer.customers,
+        addresses: state.addressesReducer.addresses,
+
+        applicationsRequestPending: state.applicationsReducer.applicationsRequestPending,
+        applicationsRequestFailed: state.applicationsReducer.applicationsRequestFailed,
+        applicationsRequestSuccess: state.applicationsReducer.applicationsRequestSuccess,
+
+        customersRequestPending: state.customersReducer.customersRequestPending,
+        customersRequestFailed: state.customersReducer.customersRequestFailed,
+        customersRequestSuccess: state.customersReducer.customersRequestSuccess,
+
+        productsRequestPending: state.productsReducer.productsRequestPending,
+        productsRequestFailed: state.productsReducer.productsRequestFailed,
+        productsRequestSuccess: state.productsReducer.productsRequestSuccess,
+
+        ordersRequestPending: state.ordersReducer.ordersRequestPending,
+        ordersRequestFailed: state.ordersReducer.ordersRequestFailed,
+        ordersRequestSuccess: state.ordersReducer.ordersRequestSuccess,
+
+        addressesRequestPending: state.addressesReducer.addressesRequestPending,
+        addressesRequestFailed: state.addressesReducer.addressesRequestFailed,
+        addressesRequestSuccess: state.addressesReducer.addressesRequestSuccess,
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(applicationsActions, dispatch)
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ApplicationsContainer);
---
+++ src/components/AddressesRender.js
@@ -0,0 +1,122 @@
+"use strict"
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import ModalButton from './modals/ModalButton';
+import { MDBDataTable } from 'mdbreact';
+import { ADDRESS_CHANGE, MAIL_FORWARDING, PACKAGE_SUBMISSION } from '../constants/applicationTypes';
+
+export function getApplicationTypeName(applicationType) {
+  switch (applicationType) {
+    case ADDRESS_CHANGE:
+      return "Address Change";
+    case MAIL_FORWARDING:
+      return "Mail Forwarding";
+    case PACKAGE_SUBMISSION:
+      return "Package Submission";
+    default:
+      return applicationType;
+  }
+}
+
+const ApplicationsRender = ({ applications, handleUpdate, handleDelete, handleCreate }) => {
+
+  function getAppBodyContent() {
+    return applications.map(obj => {
+      // Deep Clone object to avoid adding to it while mapping over it during map
+      let newObj = JSON.parse(JSON.stringify(obj))
+      const appTypeLabel = getApplicationTypeName(obj.ss_applicationtype);
+
+      newObj["view"] = (
+        <ModalButton CRUDOption="View" label="View" name={obj.ss_name} entity="Application" 
+        initialValues={{ ...obj, appTypeLabel: appTypeLabel }}  />
+      );
+      newObj["delete"] = (
+        <ModalButton CRUDOption="Update" label="Update" name={obj.ss_name} entity="Application" 
+        initialValues={{ ...obj, appTypeLabel: appTypeLabel }}  onSubmit={(values) => handleUpdate(values, obj)} />
+      );
+      newObj["update"] = (
+        <ModalButton CRUDOption="Delete" label="Delete" name={obj.ss_name} entity="Application" 
+        initialValues={{ ...obj, appTypeLabel: appTypeLabel }}  onSubmit={() => handleDelete(obj)} />
+      );
+
+      newObj["appTypeLabel"] = appTypeLabel;
+      return newObj;
+    });
+  }
+
+  let data = {
+    columns: [
+      {
+        label: 'ID',
+        field: 'ss_applicationid',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'App Name',
+        field: 'ss_name',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'Type',
+        field: 'appTypeLabel',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'Created On',
+        field: 'createdon',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: '',
+        field: 'view',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: '',
+        field: 'delete',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: '',
+        field: 'update',
+        sort: 'asc',
+        width: 150
+      },
+
+    ],
+    rows: getAppBodyContent()
+
+
+  }
+  return (
+    <div>
+      <h1>Applications</h1>
+      <ModalButton CRUDOption="Create" label="Create New Application" name={`Application ${applications.length}`} entity="Application" 
+      initialValues={{ ss_name: `Application ${applications.length}` }} onSubmit={(values) => handleCreate(values)} />
+      <MDBDataTable
+        striped
+        bordered
+        small
+        responsive
+        data={data}
+      />
+    </div>
+  );
+}
+
+ApplicationsRender.propTypes = {
+  applications: PropTypes.array,
+  handleCreate: PropTypes.func,
+  handleView: PropTypes.func,
+  handleUpdate: PropTypes.func,
+  handleDelete: PropTypes.func
+};
+
+export default ApplicationsRender;
